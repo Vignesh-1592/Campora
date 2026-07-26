9e64711d7ca47d519440ffc9bdcefb5811b63f41
Vignesh <vignesh@gmail.com>
2026-07-26 12:54:03 +0530

    Connected backend with MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load Environment Variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
@@ -10,8 +18,9 @@ app.get("/", (req, res) => {
     res.send("🚀 Campora Backend Running Successfully!");
 });
 
-const PORT = 5000;
+// Read PORT from .env
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
